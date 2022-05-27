b7f52f5b2e50fd04735a2f18901f89822d69fe5f
TenViki <viktorkomarek28@gmail.com>
2022-05-27 12:13:44 +0200

    :constrction: Starts work on spell command

--- src/commands/spell.command.ts
+++ src/commands/spell.command.ts
@@ -1,7 +1,64 @@
-import { prefix } from "../commands";
+import { prefix, sendError } from "../commands";
 import { Message } from "discord.js";
 import { CommandHandler } from "../types/commandHandler";
 import axios from "axios";
+import { Damage, Dc, Spell } from "../types/spell.type";
+import { createEmbed } from "../utils/embed";
+
+const formatSpellName = (name: string) =>
+  name.toLocaleLowerCase().replaceAll(" ", "-");
+
+const formatSpell = (spell: Spell): string => {
+  const description = spell.desc
+    .join("\n\n")
+    .replace(/([0-9]+[d][0-9]+)|([0-9]+\ feet)|([0-9]+\-foot)/g, (a) => {
+      return `**${a}**`;
+    });
+  const rangeInMeters = /[0-9]+/g.exec(spell.range)?.[0];
+  console.log(rangeInMeters);
+  return `**Level:** ${spell.level}
+**Casting time:** ${spell.casting_time}
+**Range:** ${spell.range} ${
+    rangeInMeters
+      ? "(" + Math.round((parseFloat(rangeInMeters) / 3.3) * 10) / 10 + "m)"
+      : ""
+  }
+**Components:** ${spell.components.join(", ")}
+**Druation:** ${spell.concentration ? "Concentration, " : ""}${spell.duration}${
+    spell.attack_type ? `\n**Attack type:** ${spell.attack_type}` : ""
+  }${spell.school ? `\n**School:** ${spell.school.name}` : ""}
+
+${description}`;
+};
+
+const formatDamage = (damage: Damage) => {
+  const damageChar = damage.damage_at_character_level
+    ? Object.keys(damage.damage_at_character_level).map(
+        (key) => `${key}: ${damage.damage_at_character_level![key]}`
+      )
+    : null;
+
+  const damageSlot = damage.damage_at_slot_level
+    ? Object.keys(damage.damage_at_slot_level).map(
+        (key) => `${key}: ${damage.damage_at_slot_level![key]}`
+      )
+    : null;
+
+  return `**Damage type:** ${damage.damage_type.name}
+${
+  damageChar
+    ? `**Damage at character level:** \`\`\`${damageChar.join("\n")}\`\`\``
+    : ""
+}   ${
+    damageSlot
+      ? `**Damage at slot level:** \`\`\`${damageSlot.join("\n")}\`\`\``
+      : ""
+  }`;
+};
+
+const formatDC = (dc: Dc) => {
+  return ``;
+};
 
 export default class ItemCommand implements CommandHandler {
   name = () => "spell";
@@ -17,6 +74,29 @@ export default class ItemCommand implements CommandHandler {
 
   process = async (message: Message) => {
     const spellName = message.content.split(" ").slice(1).join(" ");
-    // const spell = await axios.get()
+    // try {
+    const spell = await axios.get<Spell>(
+      "https://www.dnd5eapi.co/api/spells/" + formatSpellName(spellName)
+    );
+
+    const embed = createEmbed(
+      `D&D Wiki: ${spell.data.name}`,
+      formatSpell(spell.data)
+    );
+
+    if (spell.data.higher_level.length)
+      embed.addField("At higher level", spell.data.higher_level.join("\n"));
+    if (spell.data.damage)
+      embed.addField("Damage", formatDamage(spell.data.damage));
+    if (spell.data.dc) embed.addField("DC", formatDC(spell.data.dc));
+
+    await message.channel.send({ embeds: [embed] });
+    // } catch (error) {
+    //   sendError(
+    //     message,
+    //     "Spell not found",
+    //     `Spell \`${spellName}\` not found.`
+    //   );
+    // }
   };
 }
--- src/types/spell.type.ts
+++ src/types/spell.type.ts
@@ -8,3 +8,66 @@ export interface SpellListing {
   name: string;
   url: string;
 }
+
+export interface DamageType {
+  index: string;
+  name: string;
+  url: string;
+}
+
+export interface Damage {
+  damage_type: DamageType;
+  damage_at_character_level?: { [key: string]: string };
+  damage_at_slot_level?: { [key: string]: string };
+}
+
+export interface DcType {
+  index: string;
+  name: string;
+  url: string;
+}
+
+export interface Dc {
+  dc_type: DcType;
+  dc_success: string;
+}
+
+export interface School {
+  index: string;
+  name: string;
+  url: string;
+}
+
+export interface Class {
+  index: string;
+  name: string;
+  url: string;
+}
+
+export interface Subclass {
+  index: string;
+  name: string;
+  url: string;
+}
+
+export interface Spell {
+  _id: string;
+  higher_level: any[];
+  index: string;
+  name: string;
+  desc: string[];
+  range: string;
+  components: string[];
+  ritual: boolean;
+  attack_type?: string;
+  duration: string;
+  concentration: boolean;
+  casting_time: string;
+  level: number;
+  damage?: Damage;
+  dc?: Dc;
+  school?: School;
+  classes: Class[];
+  subclasses: Subclass[];
+  url: string;
+}
